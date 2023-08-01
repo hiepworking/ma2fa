451e15988b8a30fb297e7f621fed331e2574b576
hiepworking <141106069+hiepworking@users.noreply.github.com>
2023-08-01 11:01:46 +0700

    1.0

---
+++ app.js
@@ -0,0 +1,25 @@
+﻿const express = require('express');
+const speakeasy = require('speakeasy');
+
+const app = express();
+const port = 80;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.post('/generateOTP', (req, res) => {
+    const secret = req.body.secretKey;
+
+    const token = speakeasy.totp({
+        secret: secret,
+        encoding: 'base32'
+    });
+
+    res.send({ otp: token });
+});
+
+app.listen(port, () => {
+    console.log(`MA2FA.COM running in ${port}`);
+});
